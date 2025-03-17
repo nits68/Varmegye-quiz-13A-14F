d7e55209f318d4522cfa14c58f9a57e6a467669d
nits68 <nits.laszlo@jedlik.eu>
2025-03-17 11:42:47 +0100

    add interfaces

--- src/county.interface.ts
+++ src/county.interface.ts
@@ -12,3 +12,50 @@ export interface ICounty {
     region: string;
     seat_id: number;
 }
+
+export interface ICountyFull {
+    _id: number;
+    area: number;
+    country_part: string;
+    flag_url_big: string;
+    flag_url_small: string;
+    largest_cities: number[];
+    largestCities: ILargestCity[];
+    name: string;
+    neighboring_counties: number[];
+    neighboring_countries: number[];
+    neighboringCounties: INeighboringCounty[];
+    neighboringCountries: INeighboringCountry[];
+    population: number;
+    region: string;
+    seat_id: number;
+}
+
+export interface ILargestCity {
+    _id: number;
+    area: number;
+    coat_of_arms_url: string;
+    name: string;
+    population: number;
+}
+
+export interface INeighboringCountry {
+    _id: number;
+    area: number;
+    flag_url_big: string;
+    flag_url_small: string;
+    name: string;
+    population: number;
+}
+
+export interface INeighboringCounty {
+    _id: number;
+    area: number;
+    country_part: string;
+    flag_url_big: string;
+    flag_url_small: string;
+    name: string;
+    population: number;
+    region: string;
+    seat_id: number;
+}
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,3 +3,9 @@ import { Router } from "express";
 export interface IController {
     router: Router;
 }
+
+export interface IResponse {
+    answers: string[];
+    question: string;
+    solution: string;
+}
