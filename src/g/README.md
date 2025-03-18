e4991934469d8cff9a27a73c46d6ab4951b48b01
nits68 <nits.laszlo@jedlik.eu>
2025-03-18 13:47:37 +0100

    add  14F groups

---
+++ src/g/README.md
@@ -0,0 +1,1 @@
+# G-csoport TypeScript források
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
+import { groupAcontroller } from "#a/groupA.controller.js";
 import App from "#app.js";
 import { groupBController } from "#b/groupB.controller.js";
 import { groupCController } from "#c/groupC.controller.js";
 import { cityController } from "#city.controller.js";
 import { countryController } from "#country.controller.js";
 import { countyController } from "#county.controller.js";
-import { GroupEController } from "#e/groupe.controller.js";
 import { groupDController } from "#d/groupD.controller.js";
-import { groupAcontroller } from "#a/groupA.controller.js";
+import { GroupEController } from "#e/groupe.controller.js";
 
-
-new App([new countryController(), new cityController(), new countyController(), new groupBController(), new groupCController(), new groupDController(),new GroupEController(),new groupAcontroller()]);
+new App([
+    new countryController(),
+    new cityController(),
+    new countyController(),
+    new groupBController(),
+    new groupCController(),
+    new groupDController(),
+    new GroupEController(),
+    new groupAcontroller(),
+]);
