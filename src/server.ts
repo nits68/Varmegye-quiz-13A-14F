f7a0ef532623dc45398087afa00822b8594ab074
KrupanszkyMarcell <marci.krupanszky@gmail.com>
2025-03-18 09:28:55 +0100

    orai munka

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import App from "#app.js";
 import { cityController } from "#city.controller.js";
 import { countryController } from "#country.controller.js";
 import { countyController } from "#county.controller.js";
+import { GroupEController } from "#e/groupe.controller.js";
 
-new App([new countryController(), new cityController(), new countyController()]);
+new App([new countryController(), new cityController(), new countyController(),new GroupEController()]);
